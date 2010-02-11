38b9fa31a085ff9d1151a7f081fdc1156496cea2
s1n <jswitzer@gmail.com>
2010-02-10 22:59:03 -0600

    Fixes the index to also store the digest.

--- app/src/kwic/index/ShiftedInput.java
+++ app/src/kwic/index/ShiftedInput.java
@@ -10,29 +10,38 @@ import java.util.logging.Logger;
  * Shifted input. Keeps track of it's own index and the originating input.
  */
 public class ShiftedInput {
-   public ShiftedInput(String input_) {
-       this._input = input_;
-       this._index = createIndex(input_);
-   }
 
-   public java.security.MessageDigest createIndex(String input_) {
+    public ShiftedInput(String input_) {
+        this._input = input_;
+        this._digest = createIndex(input_);
+        this._index = getIndex();
+    }
+
+    public java.security.MessageDigest createIndex(String input_) {
         try {
-            this._index = MessageDigest.getInstance("SHA-1");
+            //compute the index from a SHA-1
+            this._digest = MessageDigest.getInstance("SHA-1");
+            this._digest.reset();
         } catch (NoSuchAlgorithmException ex) {
             Logger.getLogger(ShiftedInput.class.getName()).log(Level.SEVERE, null, ex);
         }
-       this._index.digest(this._input.getBytes());
-       return _index;
-   }
+        this._digest.update(this._input.getBytes());
+        return _digest;
+    }
 
-   public String getIndex() {
-       return new BigInteger(1, this._index.digest()).toString(16);
-   }
+    public String getIndex() {
+        //recompute the index
+        if(this._index == null || this._index.length() <= 0) {
+            this._index = (new BigInteger(1, this._digest.digest())).toString(16);
+        }
+        return this._index;
+    }
 
-   @Override public String toString() {
-       return this._input;
-   }
-
-   private java.lang.String _input;
-   private java.security.MessageDigest _index;
+    @Override public String toString() {
+        return this._input;
+    }
+    private java.lang.String _input;
+    private java.lang.String _index;
+    private java.security.MessageDigest _digest;
 }
+ 
