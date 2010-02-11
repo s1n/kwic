8936294704996e5ba36fc74fb4e779e8f4b76127
s1n <jswitzer@gmail.com>
2010-02-10 23:03:28 -0600

    [index] Index input file reader.
    This version only barely acts like an iterator. Soon, it will be able
    to iterate over all of the automatically shifted input as well.

---
+++ app/src/kwic/index/InputReader.java
@@ -0,0 +1,41 @@
+package kwic.index;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Responsible for reading the input from the File.
+ */
+public class InputReader extends LineNumberReader {
+    public InputReader(Reader in) {
+        super(in);
+    }
+    
+    public InputReader(Reader in, int size) {
+        super(in, size);
+    }
+
+    public InputReader(String file_) throws FileNotFoundException {
+        this(new FileReader(new File(file_)));
+    }
+
+    public ShiftedInput next() {
+        ShiftedInput ret = null;
+        String line = "";
+        try {
+            line = this.readLine();
+            if(line != null) {
+                ret = new ShiftedInput(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(InputReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+    }
+}
