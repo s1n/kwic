de61298848b2e576dfcff53c25f3b5c60a12c63c
s1n <jswitzer@gmail.com>
2010-02-10 23:02:19 -0600

    [index] Initial shifter implementation.
    This implementation will eventually be able to keep track of
    automatically generating the shifted input strings. For now, this
    is just a boilerplate class.

---
+++ app/src/kwic/index/CircularShifter.java
@@ -0,0 +1,14 @@
+package kwic.index;
+
+/**
+ * Performs circular shifting of the input tokens.
+ */
+public class CircularShifter implements Shifter {
+    public void generatePermutations() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void generatePermutations(ShiftedInput input_) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
