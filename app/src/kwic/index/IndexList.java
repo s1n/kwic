932caf973ba7039563831f1a08e1e893f6ab14d9
s1n <jswitzer@gmail.com>
2010-02-16 22:41:15 -0600

    [index] First run of the IndexList resultset.

--- app/src/kwic/index/IndexList.java
+++ app/src/kwic/index/IndexList.java
@@ -1,9 +1,33 @@
 package kwic.index;
 
+import java.util.Collection;
+
 /**
  * Collects all the results by maintaining order to the results. This object
  * can also be iterated over to inspect the indexed results.
  */
-public class IndexList extends java.util.ArrayList<kwic.index.ShiftedInput> {
+public class IndexList extends java.util.TreeSet<kwic.index.IndexedString> {
 
+    public IndexList(Shifter cs_) {
+        this._shift = cs_;
+    }
+
+    @Override
+    public boolean add(IndexedString e_) {
+        this._shift.generatePermutations(e_);
+
+        IndexedString is;
+        while((is = this._shift.next()) != null) {
+            //FIXME don't add is if it's already in this TreeSet
+            super.add(is);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return super.remove(o);
+    }
+
+    private Shifter _shift = null;
 }
