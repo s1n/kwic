ccf0994ee0730b4f46a76125cb1324954acbea21
s1n <jswitzer@gmail.com>
2010-02-16 22:29:00 -0600

    [reorg] Rename ShiftedInput to IndexedString.
    This name change is significant because the indexed string does not
    keep track of individual tokens. The IndexedString only keeps track
    of what's necessary for indexing itself and finding derivatives.

---
+++ app/src/kwic/index/IndexedString.java
@@ -0,0 +1,82 @@
+package kwic.index;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Shifted input. Keeps track of it's own index and the originating input.
+ * @todo Remove the stored _input to compress.
+ */
+public class IndexedString implements Comparable<IndexedString> {
+
+    public IndexedString() {
+        this._index = null;
+        this._input = null;
+        this._digest = null;
+    }
+
+    public IndexedString(IndexedString pre_) {
+        this._origin_digest = pre_._digest;
+        this._origin_index = pre_._index;
+    }
+
+    public IndexedString(IndexedString pre_, String input_) {
+        if(pre_ != null) {
+           this._origin_digest = pre_._digest;
+           this._origin_index = pre_._index;
+        }
+        this._input = input_;
+        this._digest = createIndex(input_);
+        this._index = getIndex();
+    }
+
+    public IndexedString(String input_) {
+        this._input = input_;
+        this._digest = createIndex(input_);
+        this._index = getIndex();
+        this._origin_digest = this._digest;
+        this._origin_index = this._index;
+    }
+
+    public java.security.MessageDigest createIndex(String input_) {
+        try {
+            //compute the index from a SHA-1
+            this._digest = MessageDigest.getInstance("SHA-1");
+            this._digest.reset();
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(IndexedString.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this._digest.update(this._input.getBytes());
+        return _digest;
+    }
+
+    public String getIndex() {
+        //recompute the index
+        if(this._index == null || this._index.length() <= 0) {
+            this._index = (new BigInteger(1, this._digest.digest())).toString(16);
+        }
+        return this._index;
+    }
+
+    @Override public String toString() {
+        return this._input;
+    }
+
+    public String originIndex() {
+        return this._origin_index;
+    }
+
+    private java.lang.String _input;
+    private java.lang.String _index;
+    private java.lang.String _origin_index;
+    private java.security.MessageDigest _digest;
+    private java.security.MessageDigest _origin_digest;
+
+    public int compareTo(IndexedString is_) {
+        return this._input.toUpperCase().compareTo(is_.toString().toUpperCase());
+    }
+}
+ 
