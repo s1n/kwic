843559966e6019701f1ca12ea1b3bf7e2edfe4e1
s1n <jswitzer@gmail.com>
2010-02-16 22:39:25 -0600

    [index] Implement the CircularShifter.
    This class is responsible for acting like an iterator and generating
    circular shift permutations (only uniques, though that isn't done).

--- app/src/kwic/index/CircularShifter.java
+++ app/src/kwic/index/CircularShifter.java
@@ -1,14 +1,37 @@
 package kwic.index;
 
+import java.util.ArrayList;
+
 /**
  * Performs circular shifting of the input tokens.
  */
-public class CircularShifter implements Shifter {
-    public void generatePermutations() {
-        throw new UnsupportedOperationException("Not supported yet.");
+public class CircularShifter extends Shifter {
+
+    public CircularShifter() {
+        this._list = new ArrayList<IndexedString>();
     }
 
-    public void generatePermutations(ShiftedInput input_) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void generatePermutations(IndexedString input_) {
+        this.tokenize(input_.toString());
+        this._sizeof = this._tokens.size();
+        this._iter_position = 0;
     }
+
+    @Override
+    IndexedString next() {
+        if(this._iter_position >= this._sizeof) {
+            return null;
+        }
+        this._tokens.add(this._tokens.remove(0));
+        //FIXME more parent/derivative input cruft, needs cleaning up here
+        IndexedString is = new IndexedString(this._iter_position == 0 ? null : this._list.get(this._iter_position - 1), join());
+        this._list.add(is);
+        this._iter_position++;
+        return is;
+    }
+
+    private int _sizeof;
+    private int _iter_position;
+    //FIXME remove this cruft, bad idea to track last generated IndexString
+    private ArrayList<IndexedString> _list;
 }
