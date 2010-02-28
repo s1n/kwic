c0fc68917182bec25cc94ea836aad393309045b0
Cliff Halasz <miliways@gmail.com>
2010-02-27 20:48:35 -0600

    added noise words to EnglishCollator

--- app/src/kwic/index/EnglishCollator.java
+++ app/src/kwic/index/EnglishCollator.java
@@ -15,4 +15,7 @@ public class EnglishCollator extends RuleBasedCollator {
                                          "< m < M < n < N < o < O < p < P < q < Q < r < R " +
                                          "< s < S < t < T < u < U < v < V < w < W < x < X " +
                                          "< y < Y < z < Z");
+
+   //these are noise words we want to completely ignore, in either uppercase or lowercase
+   private String[] _noise = {"a", "an", "the", "and", "or", "of", "to", "be", "is", "in", "out", "by", "as", "at", "off"};
 }
