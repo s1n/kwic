dfa269bbc4a7fe9c2fd3aeab5dd89262d83bf4fb
s1n <jswitzer@gmail.com>
2010-03-23 22:08:52 -0500

    [index] First added IndexedString should be originInput (found by urltest2).

--- app/src/kwic/index/CircularShifter.java
+++ app/src/kwic/index/CircularShifter.java
@@ -32,6 +32,8 @@ public class CircularShifter extends Shifter {
       do {
          if(this._iter_position >= this._tokens.size()) {
             return null;
+         } else if(this._iter_position == 0 && !startsWithNoise()) {
+            break;
          } else if(this._iter_position >= 0) {
             this._tokens.add(this._tokens.remove(0));
          }
