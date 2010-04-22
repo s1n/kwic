9e317272a04f9ceb535bae84f8c784146173fd26
s1n <jswitzer@gmail.com>
2010-04-21 21:56:09 -0500

    [docs] Removes autogenerated docs.

--- web/src/java/kwic/frontend/IndexedStringFacadeRemote.java
+++ web/src/java/kwic/frontend/IndexedStringFacadeRemote.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package kwic.frontend;
 
 import java.util.List;
@@ -11,7 +6,6 @@ import kwic.backend.IndexedString;
 
 /**
  *
- * @author s1n
  */
 @Remote
 public interface IndexedStringFacadeRemote {
