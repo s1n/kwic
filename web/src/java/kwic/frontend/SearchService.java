7d1981edc537c15e38a0d7a89938375560d18592
s1n <jswitzer@gmail.com>
2010-04-21 22:04:26 -0500

    [frontend] Big change to the Search service.
    It can now automatically pull the JTA data from the database, since the
    IndexedString is an @Entity. This also will return search results as neat little
    URL's with the description/input as the text.

--- web/src/java/kwic/frontend/SearchService.java
+++ web/src/java/kwic/frontend/SearchService.java
@@ -2,6 +2,7 @@ package kwic.frontend;
 
 import java.util.List;
 import javax.ejb.EJB;
+import javax.naming.InitialContext;
 import kwic.backend.CircularShifter;
 import kwic.backend.IndexList;
 import kwic.backend.IndexedString;
@@ -11,45 +12,48 @@ import kwic.backend.IndexedString;
  */
 public class SearchService {
 
-    public SearchService() {
-        _index = new IndexList(new CircularShifter());
-        try {
-           List<IndexedString> stuff = _indexEJB.findAll();
-           System.err.println("size = " + stuff.size());
-           if(stuff.size() > 0) {
-               for(IndexedString is : stuff) {
-                   _index.add(is);
-               }
-           } else {
-              IndexedString parent = null;
-              IndexedString is = new IndexedString(parent, "dark side http://foo.com");
-              _index.add(is);
-              _indexEJB.create(is);
-              is = new IndexedString(parent, "yeah yeah yeahs http://yeahs.com");
-              _index.add(is);
-              _indexEJB.create(is);
-              is = new IndexedString(parent, "this project is time consuming http://timewasters.com");
-              _index.add(is);
-              _indexEJB.create(is);
-              is = new IndexedString(parent, "omg wtf bbq http://wtf.com");
-              _index.add(is);
-              _indexEJB.create(is);
-           }
-        } catch (Exception e) {
-            //drop it like it's hot
-        }
-    }
+   public SearchService() {
+      _index = new IndexList(new CircularShifter());
+      try {
+         _indexEJB = (IndexedStringFacadeRemote)new InitialContext().lookup("web/IndexedString/remote");
+         List<IndexedString> stuff = _indexEJB.findAll();
+         //System.err.println("size = " + stuff.size());
+         if(stuff.size() > 0) {
+            for(IndexedString is : stuff) {
+               _index.add(is);
+            }
+         } else {
+            //IndexedString parent = null;
+            //IndexedString is = new IndexedString(parent, "dark side http://foo.com");
+            //_index.add(is);
+            //_indexEJB.create(is);
+            //is = new IndexedString(parent, "yeah yeah yeahs http://yeahs.com");
+            //_index.add(is);
+            //_indexEJB.create(is);
+            //is = new IndexedString(parent, "this project is time consuming http://timewasters.com");
+            //_index.add(is);
+            //_indexEJB.create(is);
+            //is = new IndexedString(parent, "omg wtf bbq http://wtf.com");
+            //_index.add(is);
+            //_indexEJB.create(is);
+         }
+      } catch(Exception e) {
+         //drop it like it's hot
+      }
+   }
 
-    public String search(String for_) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Results:<br/><br/>");
-        for(IndexedString is : _index.search(for_)) {
-            sb.append(is.toString() + "<br/>");
-        }
-        //return "Searching for \"" + for_ + "\"...";
-        return sb.toString();
-    }
-    private IndexList _index;
-    @EJB
-    private static IndexedStringFacadeRemote _indexEJB;
+   public String search(String for_) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("<ul>");
+      java.util.TreeSet<kwic.backend.IndexedString> results = _index.search(for_);
+      for(IndexedString is : results) {
+         sb.append("<li><a href=\"" + is.getURL() + "\">" + is.toString() + "</a><br/>");
+      }
+      sb.append("</ul><br/><br/>");
+      sb.append("Total results found: " + results.size());
+      return sb.toString();
+   }
+   private IndexList _index;
+   @EJB
+   private static IndexedStringFacadeRemote _indexEJB;
 }
