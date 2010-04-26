8628afa3e7379dfe53ad20ef6ca59ceb28e289cc
s1n <jswitzer@gmail.com>
2010-04-25 19:04:36 -0500

    [index] fixes a bug with the number of times shifting was done with noise words present

--- web/src/java/kwic/backend/CircularShifter.java
+++ web/src/java/kwic/backend/CircularShifter.java
@@ -1,5 +1,7 @@
 package kwic.backend;
 
+import java.util.Iterator;
+
 /**
  * Class to perform circular shifting of input tokens.
  */
@@ -10,8 +12,9 @@ public class CircularShifter extends Shifter {
 
    public void generatePermutations(IndexedString input_) {
       if(this._tokens == null || this._tokens.size() == 0) {
-         this.tokenize(input_.toString());
+         this.tokenize(input_.getInput());
          this._iter_position = 0;
+         this._iter_max = this._tokens.size();
       }
    }
 
@@ -19,6 +22,7 @@ public class CircularShifter extends Shifter {
    public void clear() {
       this._origin = null;
       this._iter_position = 0;
+      this._iter_max = 0;
    }
 
    /**
@@ -30,11 +34,15 @@ public class CircularShifter extends Shifter {
    @Override
    public IndexedString next() {
       do {
-         if(this._iter_position >= this._tokens.size()) {
+         if(this._iter_position >= this._iter_max) {
             return null;
          } else if(this._iter_position == 0 && !startsWithNoise()) {
             break;
          } else if(this._iter_position >= 0) {
+            //need fewer shift results if we encounter noise
+            if(startsWithNoise()) {
+               this._iter_max--;
+            }
             this._tokens.add(this._tokens.remove(0));
          }
       } while(startsWithNoise());
@@ -47,6 +55,8 @@ public class CircularShifter extends Shifter {
       this._iter_position++;
       return is;
    }
+   private Iterator<String> _betteriter;
    private int _iter_position = 0;
+   private int _iter_max = 0;
    private IndexedString _origin = null;
 }
